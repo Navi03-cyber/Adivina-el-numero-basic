8de84465d35aa31d29a650e737b9043545d8aa2f
Navi03 <73278819+Navi03-cyber@users.noreply.github.com>
2023-10-09 22:47:05 +0200

    Update main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+#Importamos la librería random
+import random
+#asignamos "false" a la variable adivinado para controlar el while
+adivinado=False
+#generamos el aleatorio
+numero_aleatorio=random.randint(1, 10)
+#el bucle se repetira mientras adivinado sea false
+while(adivinado==False):
+    #se le pregunta al usuario el numero, guardandose en la variable respuesta
+    respuesta=int(input("Cual numero crees que estoy pensando?"))
+    
+    if(respuesta == numero_aleatorio):#Si respuesta tiene el mismo numero que nuestro aleatorio el usuario ha ganado
+        
+        print("genial me has pillado, el numero era " + str(numero_aleatorio))#str()sirve para convertir el dato int a string
+        adivinado=True
+    
+    else:
+
+        print("vamos a probar de nuevo")#si no, el bucle sigue hasta que se adivine ya que adivinado sigue en false
+        
